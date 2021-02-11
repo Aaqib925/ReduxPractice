f1077589c4d0cd699c2f6a336be3688bc44ca040
aaqib925 <muhammadaaqib925@gmail.com>
2021-02-11 16:07:12 +0500

    working

--- redux-starter/src/store/middleware/errorHandler.js
+++ redux-starter/src/store/middleware/errorHandler.js
@@ -1,5 +1,6 @@
 const errorHandler = (store) => (next) => (action) => {
   if (action.type === "error")
     console.log("Toastify: ", action.payload.message);
+  else action(next);
 };
 export default errorHandler;
